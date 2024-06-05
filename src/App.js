d1b3ca83c83dad8ee57f03c1b5579415080c5834
iTrooz <hey@itrooz.fr>
2024-06-05 08:40:38 +0200

    add chartjs-plugin-annotation + setup both graphs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Line, Chart } from 'react-chartjs-2';
+import annotationPlugin from "chartjs-plugin-annotation";
+
+// reg
+ChartJS.register(
+  annotationPlugin
+);
 
 function App() {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Sales',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      annotation: {
+        annotations: [
+          {
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y',
+            value: 70,
+            borderColor: 'red',
+            borderWidth: 2,
+            label: {
+              enabled: true,
+              content: 'Threshold',
+            },
+          },
+        ],
+      },
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ width: '50%', height: '50vh' }}>
+          <Chart type="line" data={data} options={options} />
+          <Line data={data} options={options} />
+        </div>
       </header>
     </div>
   );
